868d03a78768df5e16c6cfff3aaf06f031361f7e
jeffIshmael <jeffishmael141@gmail.com>
2024-05-18 13:40:04 +0300

    chore(Harmony) : Main Skeleton

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ButtonDemo from './components/LandingPage';
+
+function App() {
+  return (
+    <>
+      <div>
+        <ButtonDem></ButtonDem>
+
+       </div>
+    </>
+  )
+}
+
+export default App
