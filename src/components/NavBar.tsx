8be01f92c64ef6ce6abee61e8bcd6e72cdc032f6
jeffIshmael <jeffishmael141@gmail.com>
2024-05-18 14:18:35 +0300

    chore(Harmony) : Main Skeleton

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+
+    <div>
+        <h1>Harmony</h1>
+        <br />
+        <a href="">home</a>
+        <br />
+        <a href="">explore</a>
+        <br />
+        <button> Login</button>
+        <button> sign up</button>
+    </div>
+  )
+}
+
+export default NavBar
